51461dcd43f2e50780337b92567d3d67c7a17ad2
Sergio <sfarinasf22@fpcoruna.afundacion.org>
2023-09-26 10:05:45 +0200

    Archivos que aún no estaban guardados

---
+++ sprint0python/adivinanza.py
@@ -0,0 +1,15 @@
+print("¿De qué color es el caballo blanco de Santiago?")
+print("a. Negro")
+print("b. Marrón")
+print("c .Blanco")
+
+respuesta= input("Elige entre a/b/c: ")
+if respuesta=="a":
+    print("Respuesta incorrecta")
+elif respuesta=="b":
+    print("Respuesta incorrecta")
+elif respuesta=="c":
+    print("Respuesta correcta")
+else:
+    print("Esa ni siquiera era una opción")
+    
