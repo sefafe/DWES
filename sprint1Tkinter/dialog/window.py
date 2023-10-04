d480901601b8618c54910981bd3ac06da4deb44d
Sergio Fariñas <145338339+sefafe@users.noreply.github.com>
2023-10-04 09:41:35 +0200

    Tarea 2: Tkinter

---
+++ sprint1Tkinter/dialog/window.py
@@ -0,0 +1,9 @@
+from tkinter import ttk, Button
+
+class MainWindow:
+    def on_button_click(self):
+        pass
+    def __init__(self,root):
+        self.root=root
+        self.button=ttk.Button(self.root, text="Realizar acción", command=self.on_button_click)
+        self.button.pack()
