154a45165098a49dc6f332580e1d40afcbbb7f02
Sergio Fariñas <145338339+sefafe@users.noreply.github.com>
2023-10-10 13:15:31 +0200

    Tarea 6

--- sprint1Tkinter/catalog/cell.py
+++ sprint1Tkinter/catalog/cell.py
@@ -2,7 +2,11 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 class Cell:
-    def __init__(self,title,path):
-        self.title=title
-        self.path=path
-        self.image_tk=ImageTk.PhotoImage(file=self.path)
+
+    def _init_(self, title, path):
+
+        self.title = title
+        self.path = path
+        
+        resizedImage = (Image.open(self.path)).resize((100, 100), Image.Resampling.LANCZOS)
+        self.imageTk = ImageTk.PhotoImage(resizedImage)
