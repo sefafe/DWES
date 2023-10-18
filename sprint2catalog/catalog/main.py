5bfb5bf68365f8385de7aba22e1029f16ebdf434
Sergio Fariñas <145338339+sefafe@users.noreply.github.com>
2023-10-18 08:43:09 +0200

    Add files via upload

--- sprint2catalog/catalog/main.py
+++ sprint2catalog/catalog/main.py
@@ -1,7 +1,7 @@
 from tkinter import Tk
-from window import MainWindow
+from load_window import LoadingWindow
 
 if __name__== "__main__":
     root=Tk()
-    app=MainWindow(root)
+    app=LoadingWindow(root)
     root.mainloop()
