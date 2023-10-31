20c5881e8d46f2ab8d7d75628064797f6a8ce284
Sergio <sfarinasf22@fpcoruna.afundacion.org>
2023-10-31 10:28:29 +0100

    Sprint3: Tarea 3

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -3,12 +3,31 @@ package com.example.mycatalog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
-
+private TextView title, description;
+private Button button1;
+private ImageView image1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        title=findViewById(R.id.title_view);
+        description=findViewById(R.id.description_view);
+        button1=findViewById(R.id.button1);
+        image1=findViewById(R.id.image1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DetailActivity.this,"Te encanta este gato",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
