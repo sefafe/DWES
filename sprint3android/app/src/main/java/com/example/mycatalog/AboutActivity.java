fa01602484380debb7db725ccff81a65e4a2a265
Sergio <sfarinasf22@fpcoruna.afundacion.org>
2023-11-20 12:16:57 +0100

    Más cambios

--- sprint3android/app/src/main/java/com/example/mycatalog/AboutActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/AboutActivity.java
@@ -1,2 +1,16 @@
-package com.example.mycatalog;public class AboutActivity {
+package com.example.mycatalog;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        textView = findViewById(R.id.text3);
+    }
 }
--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,19 +1,37 @@
 package com.example.mycatalog;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class CatalogActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+
+public class CatalogActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 private Button button1;
+private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
         button1=findViewById(R.id.button1);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +40,40 @@ private Button button1;
                 startActivity(intent);
             }
         });
+
+
     }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_catalog) {
+            // Aquí puedes manejar el clic en "Catalog Activity"
+            Intent intentCatalog = new Intent(this, CatalogActivity.class);
+            startActivity(intentCatalog);
+        } else if (id == R.id.nav_about) {
+            // Aquí puedes manejar el clic en "About Activity"
+            Intent intentAbout = new Intent(this, AboutActivity.class);
+            startActivity(intentAbout);
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    
+
+
+
 
 }
